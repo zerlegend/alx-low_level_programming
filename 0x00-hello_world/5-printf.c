1ec97921c00240e9d7102b520fb09ae16aa479d2
zerlegend <zerlegend@gmail.com>
2022-12-08 10:28:39 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
- *main print the message using printf
- *
- */ 
+/**
+* main - main block, this function should print the sentence
+* Return: 0
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");

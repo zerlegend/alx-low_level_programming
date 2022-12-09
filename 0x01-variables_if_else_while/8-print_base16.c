b75f92139f49dfe99f51904d9dff161c14a370ff
zerlegend <zerlegend@gmail.com>
2022-12-09 09:49:39 -0800

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 
+ */
+int main(void)
+
+{
+	char b_16;
+
+	for (b_16 = 48; b_16 <= 57; b_16++)
+	{putchar(b_16);
+	}
+
+	for (b_16 = 97 ; b_16 <= 102; b_16++)
+	{
+		putchar(b_16);
+	}
+
+	putchar ('\n');
+	return (0);
+
+}

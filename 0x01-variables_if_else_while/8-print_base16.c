5df27d8da29f6a67911c2210ff2f2a72264d6e5a
zerlegend <zerlegend@gmail.com>
2022-12-09 10:01:41 -0800

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,19 +6,15 @@
  */
 int main(void)
 {
-	char b_16;
-
-	for (b_16 = 48; b_16 <= 57; b_16++)
-	{
-		putchar(b_16);
-	}
-
-	for (b_16 = 97 ; b_16 <= 102; b_16++)
-	{
-		putchar(b_16);
-	}
-
-	putchar ('\n');
-	return (0);
-
+char b_16;
+for (b_16 = 48; b_16 <= 57; b_16++)
+{
+putchar(b_16);
+}
+for (b_16 = 97 ; b_16 <= 102; b_16++)
+{
+putchar(b_16);
+}
+putchar ('\n');
+return (0);
 }

8d4b5db2c37e744f1a3d07b341b913e115377d90
zerlegend <zerlegend@gmail.com>
2022-12-09 09:58:16 -0800

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
  * main - entry point
- *
- * Return: 0 
+ * hexadecimal
+ * Return: zero 
  */
 int main(void)
-
 {
 	char b_16;
 
 	for (b_16 = 48; b_16 <= 57; b_16++)
-	{putchar(b_16);
+	{
+		putchar(b_16);
 	}
 
 	for (b_16 = 97 ; b_16 <= 102; b_16++)

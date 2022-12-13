764466f7c5c79fe00b836a149dc7ee69388a8b3d
zerlegend <zerlegend@gmail.com>
2022-12-13 09:26:54 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@
 
 void print_alphabet_x10(void)
 {
-	int line, ch;
-	
-	for (line = 0; line <= 9; ++line)
-		{
-			for (ch = 'a'; ch <= 'z'; ++ch)
-				_putchar(ch);
-			_putchar('\n');
-		}
+int line, ch;
+
+for (line = 0; line <= 9; ++line)
+{
+for (ch = 'a'; ch <= 'z'; ++ch)
+_putchar(ch);
+_putchar('\n');
+}
 }

1143b816358ff09f94da8611c9e0d6ebaa54d968
zerlegend <zerlegend@gmail.com>
2022-12-13 13:54:14 -0800

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,43 +1,26 @@
 #include "main.h"
 
 /**
- * print_times_table - prints time table of n
+ * main - Entry point
  *
- * @n: takes number input
+ * Description: computes the sum of all
+ *		the multiples of 3 or 5
+ *		below 1024 (excluded)
+ *
+ * Return: Always 0 (Success)
  */
 
-void print_times_table(int n)
+int main(void)
 {
-int prod, mult, num;
+int sum, num;
+for (num = 0; num < 1024; ++num)
+{
+if ((num % 3 == 0) || (num % 5 == 0))
+{
+sum += num;
+}
+}
+printf("%d\n", sum);
 
-if (n <= 15 && n >= 0)
-{for (num = 0; num <= n; ++num)
-{
-_putchar(48);
-for (mult = 1; mult <= n; ++mult)
-{
-_putchar(',');
-_putchar(' ');
-prod = num * mult;
-if (prod <= 9)
-{
-_putchar(' ');
-}
-if (prod <= 99)
-{
-_putchar(' ');
-}
-if (prod >= 100)
-{
-_putchar((prod / 100) + 48);
-_putchar((prod / 10) % 10 + 48);
-}
-else if (prod <= 99 && prod >= 10)
-{
-_putchar((prod / 10) + 48);
-_putchar((prod % 10) + 48);
-}
-_putchar('\n');
-}
-}
+return (0);
 }

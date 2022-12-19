7a999eda78d91c67b9388c1269eaa8874df4463b
zerlegend <zerlegend@gmail.com>
2022-12-19 14:03:01 -0800

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -10,10 +9,17 @@
  */
 int main(void)
 {
-	int num;
+	int pass, sum;
 
-	srand(time(0));
-	num = rand();
-	printf("%i\n", num);
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+
+	printf("%c", 2772 - sum);
 	return (0);
 }

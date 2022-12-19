da57e923434ac5ad4d8a92d57436807bb69cea32
zerlegend <zerlegend@gmail.com>
2022-12-19 10:28:35 -0800

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/*
- *_puts - prints a string
- *@str: A pointer to an int that will be changed
- *
- *Return: void which means our answer is correct
- */
 
+/**
+ * _puts - prints a string
+ * @str: A pointer to an int that will be changed
+ *
+ * Return: void which means our answer is correct
+ */
 void _puts(char *str)
 {
 	char *c;

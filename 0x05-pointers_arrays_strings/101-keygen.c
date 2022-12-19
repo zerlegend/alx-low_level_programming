15354254b52a767e782a7d9b045d046e60040c3f
zerlegend <zerlegend@gmail.com>
2022-12-19 13:40:44 -0800

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	srand(time(0));
+	num = rand();
+	printf("%i\n", num);
+
+	return (0);
+}

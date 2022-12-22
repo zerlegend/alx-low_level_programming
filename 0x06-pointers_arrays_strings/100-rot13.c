5fcc7f58b114c4beaf2ac6928a9f6e5f4502221f
zerlegend <zerlegend@gmail.com>
2022-12-22 12:15:47 -0800

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,17 +8,17 @@
 
 char *rot13(char *s)
 {
-int i, ii;
+int i, j;
 char input[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 for (i = 0; s[i] != '\0'; i++)
 {
-for (ii = 0; ii < 54; ii++)
+for (j = 0; ii < 52; j++)
 {
-if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A')) && s[i] == input[ii])
+if (s[i] == input[j])
 {
-s[i] = output[ii];
+s[i] = output[j];
 break;
 }
 }

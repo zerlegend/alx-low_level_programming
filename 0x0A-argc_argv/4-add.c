2a7458bff5eae4948037c0d7d7f12948f726dfaa
zerlegend <zerlegend@gmail.com>
2023-01-06 08:13:20 -0800

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,10 +7,10 @@
  * check_num - check - string there are digit
  * @str: array str
  *
- * Return; Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int check_num(char *str)
+
 {
 	/*Declaring variables*/
 	unsigned int count;
@@ -32,7 +32,7 @@ int check_num(char *str)
  * @argc: Count arguments
  * @argv: Arguments
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
@@ -45,10 +45,9 @@ int main(int argc, char *argv[])
 	count = 1;
 	while (count < argc) /*Goes through the whole array*/
 	{
-		if(check_num(argv[count]))
+		if (check_num(argv[count]))
 		{
-			str_to_int = atoi(argv[count]); 
-			/*ATOI --> convert string to int*/
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
 		}
 		/*Condition if one of the number contains symbols that are not digits*/

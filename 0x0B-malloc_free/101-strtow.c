bc04d46ebda19e783367a8a813c15089172f2a4c
zerlegend <zerlegend@gmail.com>
2023-01-11 12:02:18 -0800

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -103,7 +103,7 @@ char **strtow(char *str)
 		if (str[i] != ' ')
 		{
 			pos = _strlen(str, i);
-			tmp[j] = malloc(sizeof(char) * (pos + 7));
+			tmp[j] = malloc(sizeof(char) * (pos + 4));
 			if (tmp[j] == NULL)
 			{
 				for (t = j; t >= 0; t--)

147e58f8bcb939d890286eb6bf04ecc7454f828e
zerlegend <zerlegend@gmail.com>
2023-01-12 11:46:20 -0800

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,15 +8,20 @@
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3){
+	unsigned long mul;
+	int i, j;
+
+	if (argc != 3)
+	{
 		printf("Error\n");
 		exit(98);
 	}
-	for (i = 1; i < argc; i++){
-		for (j = 0; argv[i][j] != '\0'; j++){
-			if (argv[i][j] > 57 || argv[i][j] < 48){
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
 				printf("Error\n");
 				exit(98);
 			}
@@ -24,5 +29,5 @@ int i, j;
 	}
 	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
-return (0);
+	return (0);
 }
